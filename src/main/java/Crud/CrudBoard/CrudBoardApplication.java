2a0ed3b78a68bcf73ebd1c34542512597ea20eec
jung su young <j9972@naver.com>
2023-01-24 17:11:50 +0900

    setup the project

---
+++ src/main/java/Crud/CrudBoard/CrudBoardApplication.java
@@ -0,0 +1,13 @@
+package Crud.CrudBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Crud/CrudBoard/CrudBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package Crud.CrudBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
