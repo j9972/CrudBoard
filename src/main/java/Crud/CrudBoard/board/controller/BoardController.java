60d5ffb9f42930062b7d0edf3e1adff84a5dc6da
jung su young <j9972@naver.com>
2023-01-25 21:18:42 +0900

    setting of templates html files

---
+++ src/main/java/Crud/CrudBoard/board/controller/BoardController.java
@@ -0,0 +1,14 @@
+package Crud.CrudBoard.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+    @GetMapping("/save")
+    public String save() {
+        return "save";
+    }
+}
---
+++ src/main/java/Crud/CrudBoard/board/controller/HomeController.java
@@ -0,0 +1,12 @@
+package Crud.CrudBoard.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
